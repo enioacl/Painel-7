8fcbeb4dd940d19816f74b68bd5b9a8cad1cea8e
enioacl <62030326+enioacl@users.noreply.github.com>
2020-03-10 16:02:59 -0300

    Rename meta 3 perguntas final.R to meta 3.R

--- Meta 3/meta 3.R
+++ Meta 3/meta 3.R
@@ -1,8 +1,8 @@
 # 'dataset' tem os dados de entrada para este script
 # 'dataset' tem os dados de entrada para este script
-##neste código o 'mes' na parte da combinação das unidades e meses está apenas para teste
-##em comentário está a função correta para ser utilizada. Ou, se for melhor, colocar
-## de 1 até 12.
+##neste cÃ³digo o 'mes' na parte da combinaÃ§Ã£o das unidades e meses estÃ¡ apenas para teste
+##em comentÃ¡rio estÃ¡ a funÃ§Ã£o correta para ser utilizada. Ou, se for melhor, colocar
+## de 1 atÃ© 12.
 # 'dataset' tem os dados de entrada para este script
 
 require(readxl)
@@ -10,28 +10,28 @@ require(dplyr)
 dados<-dataset 
 
 dados<-dados%>%select(sort(names(.)))
-names(dados)<-c("mês","Pergunta","quant","unidade")
-dados<-dados%>%select(unidade,mês,Pergunta,quant)
+names(dados)<-c("mÃªs","Pergunta","quant","unidade")
+dados<-dados%>%select(unidade,mÃªs,Pergunta,quant)
 unidade<-as.data.frame(dados[c(1:37,39),1])
 dados$quant<-as.numeric(dados$quant)
 dados$quant[is.na(dados$quant)]=0
-dados$mês<-as.numeric(dados$mês)
+dados$mÃªs<-as.numeric(dados$mÃªs)
 
-d35<-dados%>%filter(Pergunta=='P35')%>%select(unidade,mês,quant)
+d35<-dados%>%filter(Pergunta=='P35')%>%select(unidade,mÃªs,quant)
 names(d35)[3]<-"P35"
-d36<-dados%>%filter(Pergunta=='P36')%>%select(unidade,mês,quant)
+d36<-dados%>%filter(Pergunta=='P36')%>%select(unidade,mÃªs,quant)
 names(d36)[3]<-"P36"
 
 
-dados2<-full_join(d35,d36,by=c("unidade","mês"))
+dados2<-full_join(d35,d36,by=c("unidade","mÃªs"))
 dados2[is.na(dados2)]=0
 
 dados2<-as.data.frame(dados2)
 
 
-TRT_1=cbind(unidade=rep(".TRT 7 1ª INSTÂNCIA",length(unique(dados2$mês))),
-            (dados2%>%group_by(mês)%>%summarise(P35=sum(P35),P36=sum(P36))))
-dados2<-dados2%>%arrange(unidade,mês)
+TRT_1=cbind(unidade=rep(".TRT 7 1Âª INSTÃ‚NCIA",length(unique(dados2$mÃªs))),
+            (dados2%>%group_by(mÃªs)%>%summarise(P35=sum(P35),P36=sum(P36))))
+dados2<-dados2%>%arrange(unidade,mÃªs)
 dados2=as.data.frame(rbind(dados2,TRT_1))
 
 
@@ -39,9 +39,9 @@ require(lubridate)
 require(data.table)
 
 
-#mes=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
+#mes=1:month(floor_date(Sys.Date() - months(1), "month")) # atÃ© o mÃªs anterior ao atual
 mes=1:12 
-combin=CJ(unidade[,1],mes) #combinação das unidades com cada mês para a comparação
+combin=CJ(unidade[,1],mes) #combinaÃ§Ã£o das unidades com cada mÃªs para a comparaÃ§Ã£o
 combin$p35=rep(0,dim(combin)[1])
 combin$p36=rep(0,dim(combin)[1])
 names(combin)=names(dados2)
@@ -53,8 +53,8 @@ require(prodlim)
 pos=which(is.na(row.match(combin[,1:2],dados2[,1:2]))) #linhas para adicionar 
 ee=rbind(dados2,combin[pos,]) #juntando o data frame com as linhas faltantes
 
-#reorganizar as linhas por unidade e mês
-ee=ee%>%arrange(unidade,mês)
+#reorganizar as linhas por unidade e mÃªs
+ee=ee%>%arrange(unidade,mÃªs)
 
 dados2=as.data.frame(ee)
 
@@ -75,14 +75,14 @@ dados2=dados2%>%mutate(Iconc_Mensal=P35/P36)
 dados2=as.data.frame(dados2%>%group_by(unidade)%>%mutate(Iconc_Acumulado=cumsum(P35)/cumsum(P36)))
 dados2[is.na(dados2)]=0
 
-meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+meses=c("janeiro","fevereiro","marÃ§o","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
 
 
 aux=1:dim(dados2)[1]
 
 
-for(i in 1:max(dados2$mês)){
-  a=which(dados2$mês==i)
+for(i in 1:max(dados2$mÃªs)){
+  a=which(dados2$mÃªs==i)
   aux[a]=meses[i]
 }
 
@@ -90,6 +90,6 @@ for(i in 1:max(dados2$mês)){
 dados2$mes_nomes=aux
 
 
-dados2$Instância<-"TRT total"
+dados2$InstÃ¢ncia<-"TRT total"
 
 dados2<-as.data.frame(dados2)

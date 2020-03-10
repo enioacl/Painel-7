c97f247e26974975a428cddadd41e65f35f03fe9
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-03-10 16:49:21 -0300

    Add files via upload

---
+++ Meta 6/meta 6 perguntas final.R
@@ -0,0 +1,104 @@
+# 'dataset' tem os dados de entrada para este script
+
+require(readxl)
+require(dplyr)
+dados<-dataset
+dados<-read_excel("X:/SGE/GABINETE/CONSELHO NACIONAL DE JUSTICA/METAS NACIONAIS CNJ/Metas Nacionais 2019 - CNJ/SQL metas/metas por pergunta/Meta 6/tab dados.xlsx")
+dados<-dados%>%select(sort(names(.)))
+
+
+names(dados)<-c("Instância","mês","Pergunta","quantidade","unidade")
+dados$mês<-as.numeric(dados$mês)
+dados$quantidade<-as.numeric(dados$quantidade)
+unidade<-dados%>%select(unidade)
+unidade<-as.data.frame(unidade[c(1:37,39),])
+
+#adicionado o "trt 1ª instância" nas perguntas únicas
+
+
+pp61<-dados%>%filter(Pergunta=='P61')%>%select(unidade,quantidade)
+names(pp61)[2]="P61"
+pp61[nrow(pp61)+1,]=c(".TRT 7 1ª INSTÂNCIA",sum(pp61$P61))
+pp61$P61<-as.numeric(pp61$P61)
+pp62<-dados%>%filter(Pergunta=='P62')%>%select(unidade,mês,quantidade)
+names(pp62)[3]="P62"
+pp63<-dados%>%filter(Pergunta=='P63')%>%select(unidade,mês,quantidade)
+names(pp63)[3]="P63"
+pp64<-dados%>%filter(Pergunta=='P64')%>%select(unidade,mês,quantidade)
+names(pp64)[3]="P64"
+pp65<-dados%>%filter(Pergunta=='P65')%>%select(unidade,quantidade)
+names(pp65)[2]="P65"
+pp65[nrow(pp65)+1,]=c(".TRT 7 1ª INSTÂNCIA",sum(pp65$P65))
+pp65$P65<-as.numeric(pp65$P65)
+
+dados2<-full_join(pp62,pp63,by=c("unidade","mês"))%>%full_join(.,pp64,by=c("unidade","mês"))
+dados2<-as.data.frame(dados2)
+dados2[is.na(dados2)]=0
+
+
+dados2<-dados2%>%arrange(unidade,mês)
+
+
+TRT_1<-dados2%>%group_by(mês)%>%summarize(P62=sum(P62), P63=sum(P63), P64=sum(P64))
+TRT_1<-as.data.frame(TRT_1%>%mutate(unidade=".TRT 7 1ª INSTÂNCIA"))
+TRT_1<-arrange(TRT_1,mês)
+dados2<-rbind(dados2,TRT_1)
+
+require(lubridate)
+require(data.table)
+
+#mes=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
+meses=1:12
+combin=CJ(unidade=unidade[,1],mês=meses) #combinação das unidades com cada mês para a comparação
+combin$p62=rep(0,dim(combin)[1])
+combin$p63=rep(0,dim(combin)[1])
+combin$p64=rep(0,dim(combin)[1])
+names(combin)=names(dados2)
+combin=as.data.frame(combin)
+
+
+require(prodlim)
+
+pos=which(is.na(row.match(combin[1:2],dados2[1:2]))) #linhas para adicionar 
+ee=rbind(dados2,combin[pos,]) #juntando o data frame com as linhas faltantes
+
+#reorganizar as linhas por unidade e mês
+ee=ee%>%arrange(unidade,mês)
+
+dados2=as.data.frame(ee)
+
+#Perguntas únicas
+dados2=left_join(dados2,pp61,by="unidade")
+dados2=left_join(dados2,pp65,by="unidade")
+dados2[is.na(dados2)]=0
+
+
+#Grau de cumprimento acumulado
+dados2<-dados2%>%group_by(unidade)%>%mutate(GC_acumulado=(cumsum(P64)+P65)/(P61+P65+cumsum(P62)-cumsum(P63))*(10/9.8))
+
+
+#Grau de cumprimento mensal
+dados2<-dados2%>%mutate(GC_mensal=(P64+P65)/(P61+P65+P62-P63)*(10/9.8))
+
+dados2$GC_acumulado[is.infinite(dados2$GC_acumulado)]<-1
+dados2$GC_mensal[is.infinite(dados2$GC_mensal)]<-1
+dados2$GC_acumulado[is.na(dados2$GC_acumulado)]<-1
+dados2$GC_mensal[is.na(dados2$GC_mensal)]<-1
+
+#Grau de cumprimento atual
+dados2=dados2%>%group_by(unidade)%>%mutate(GC_atual=last(GC_acumulado))
+
+
+
+meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+aux2=1:dim(dados2)[1]
+
+for(i in 1:max(dados2$mês)){
+  a=which(dados2$mês==i)
+  aux2[a]=meses[i]
+}
+
+dados2$mes_nomes=aux2
+
+dados2<-as.data.frame(dados2)
+
---
+++ Meta 6/script resumo meta 6.R
@@ -0,0 +1,146 @@
+#Resumo meta 6
+
+require(dplyr)
+require(readxl)
+dados<-dataset
+dados<-dados%>%select(sort(names(.)))
+names(dados)=c("Instância","mês","Pergunta","quant","unidade")
+dados<-dados%>%select(-unidade)
+dados$mês<-as.numeric(dados$mês)
+dados$quant<-as.numeric(dados$quant); dados$quant[is.na(dados$quant)]=0
+
+
+
+################################################################
+#primeira instância
+
+inst1_p61<-as.numeric(dados%>%filter(Instância=="Primeira" & Pergunta=="P61")%>%select(quant)%>%summarise(sum(quant)))
+
+inst1_p62<-as.data.frame(dados%>%filter(Instância=="Primeira" & Pergunta=="P62")%>%group_by(mês)%>%summarise(quant=sum(quant))%>%select(mês,quant))
+names(inst1_p62)=c("mês","P62")
+
+inst1_p63<-dados%>%filter(Instância=='Primeira' & Pergunta=='P63')%>%group_by(mês)%>%summarise(quant=sum(quant))%>%select(mês,quant)
+names(inst1_p63)=c("mês","P63")
+
+
+inst1_p64<-dados%>%filter(Instância=="Primeira" & Pergunta=="P64")%>%group_by(mês)%>%summarise(quant=sum(quant))%>%select(mês,quant)
+names(inst1_p64)=c("mês","P64")
+
+inst1_p65<-as.numeric(dados%>%filter(Instância=='Primeira' & Pergunta=='P65')%>%select(quant)%>%summarise(sum(quant)))
+
+
+primeira_inst<-full_join(inst1_p62,inst1_p63,by="mês")%>%full_join(.,inst1_p64,by="mês")
+primeira_inst[is.na(primeira_inst)]=0
+
+primeira_inst<-as.data.frame(primeira_inst%>%group_by(mês)%>%summarise(P62=sum(P62),P63=sum(P63),P64=sum(P64)))%>%ungroup()
+primeira_inst<-arrange(primeira_inst,mês)
+
+
+require(lubridate)
+require(data.table)
+##############################
+############################
+#####meses=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
+meses<-1:12
+combin<-data.frame(mês=meses,P62=rep(0,length(meses)),P63=rep(0,length(meses)),P64=rep(0,length(meses)))
+
+require(prodlim)
+pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(primeira_inst$mês))))
+primeira_inst<-rbind(primeira_inst,combin[pos,])%>%arrange(mês)
+primeira_inst$P61<-inst1_p61
+primeira_inst$P65<-inst1_p65
+primeira_inst$Instância<-"Primeira"
+
+
+
+#Grau de cumprimento acumulado
+primeira_inst=primeira_inst%>%mutate(GC_acumulado=(cumsum(P64)+P65)/(P61+P65+cumsum(P62)-cumsum(P63))*(10/9.8))%>%arrange(mês)
+
+#Graud e cumprimento mensal
+primeira_inst$GC_mensal=((primeira_inst$P64+primeira_inst$P65)/(primeira_inst$P61+primeira_inst$P65+primeira_inst$P62-primeira_inst$P63)*(10/9.8))
+
+#Grau de cumprimento atual
+primeira_inst<-primeira_inst%>%mutate(GC_atual=last(GC_acumulado))
+
+
+
+#################################################################
+#Segunda instância
+inst2_p61<-as.numeric(dados%>%filter(Instância=='Segunda' & Pergunta=='P61')%>%select(quant))
+inst2_p62<-dados%>%filter(Instância=='Segunda' & Pergunta=='P62')%>%group_by(mês)%>%summarise(quant=sum(quant))%>%select(mês,quant)
+names(inst2_p62)<-c("mês","P62")
+
+
+inst2_p63<-dados%>%filter(Instância=='Segunda' & Pergunta=='P63')%>%group_by(mês)%>%summarise(quant=sum(quant))%>%select(mês,quant)
+names(inst2_p63)<-c("mês","P63") 
+
+
+inst2_p64<-dados%>%filter(Instância=='Segunda' & Pergunta=='P64')%>%group_by(mês)%>%summarise(quant=sum(quant))%>%select(mês,quant)
+names(inst2_p64)<-c("mês","P64")
+
+inst2_p65<-as.numeric(dados%>%filter(Instância=='Segunda' & Pergunta=='P65')%>%select(quant))
+
+
+segunda_inst<-full_join(inst2_p62,inst2_p63,by="mês")%>%full_join(.,inst2_p64,by="mês")
+segunda_inst[is.na(segunda_inst)]<-0
+
+
+pos2=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(segunda_inst$mês))))
+segunda_inst<-rbind(segunda_inst,combin[pos2,])%>%arrange(mês)
+segunda_inst$P61<-inst2_p61
+segunda_inst$P65<-inst2_p65
+segunda_inst$Instância<-"Segunda"
+
+
+
+#Grau de cumprimento acumulado
+segunda_inst<-segunda_inst%>%mutate(GC_acumulado=(cumsum(P64)+P65)/(P61+P65+cumsum(P62)-cumsum(P63))*(10/9.8))
+
+#Grau de cumprimento mensal
+segunda_inst$GC_mensal<-((segunda_inst$P64+segunda_inst$P65)/(segunda_inst$P61+segunda_inst$P65+segunda_inst$P62-segunda_inst$P63)*(10/9.8))
+
+#Grau de cumprimento atual
+segunda_inst<-segunda_inst%>%mutate(GC_atual=last(GC_acumulado))
+
+
+
+##########################################################
+#TRT total
+trt_total<-rbind(primeira_inst%>%select(mês,P61,P62,P63,P64,P65),segunda_inst%>%select(mês,P61,P62,P63,P64,P65))
+trt_total<-as.data.frame(trt_total%>%group_by(mês)%>%summarise(P62=sum(P62),P63=sum(P63),P64=sum(P64),P61=sum(P61),P65=sum(P65))%>%ungroup()%>%arrange(mês))
+trt_total$Instância<-"TRT total"
+
+
+#Grau de cumprimento acumulado
+trt_total<-trt_total%>%mutate(GC_acumulado=(cumsum(P64)+P65)/(P61+P65+cumsum(P62)-cumsum(P63))*(10/9.8))
+
+#Grau de cumprimento mensal
+trt_total$GC_mensal<-((trt_total$P64+trt_total$P65)/(trt_total$P61+trt_total$P65+trt_total$P62-trt_total$P63)*(10/9.8))
+
+#Grau de cumprimento atual
+trt_total<-trt_total%>%mutate(GC_atual=last(GC_acumulado))
+
+
+#BD final
+resumo_meta6<-rbind(primeira_inst,segunda_inst,trt_total)
+
+resumo_meta6$GC_acumulado[is.infinite(resumo_meta6$GC_acumulado)]<-1
+resumo_meta6$GC_mensal[is.infinite(resumo_meta6$GC_mensal)]<-1
+resumo_meta6$GC_acumulado[is.na(resumo_meta6$GC_acumulado)]<-1
+resumo_meta6$GC_mensal[is.na(resumo_meta6$GC_mensal)]<-1
+resumo_meta6$GC_atual[is.infinite(resumo_meta6$GC_atual)]<-1
+resumo_meta6$GC_atual[is.na(resumo_meta6$GC_atual)]<-1
+
+meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+
+
+aux=1:dim(resumo_meta6)[1]
+
+
+for(i in 1:max(resumo_meta6$mês)){
+  a=which(resumo_meta6$mês==i)
+  aux[a]=meses[i]
+}
+
+
+resumo_meta6$mes_nomes=aux

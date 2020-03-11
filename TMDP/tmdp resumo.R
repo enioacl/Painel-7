7e60520bd5ee0d9e5e93da4cc37ec1b5b0431926
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-03-11 12:44:52 -0300

    Add files via upload

---
+++ TMDP/tmdp resumo.R
@@ -0,0 +1,106 @@
+# 'dataset' tem os dados de entrada para este script
+require(dplyr)
+
+require(prodlim)
+require(data.table)
+require(lubridate)
+
+dados<-dataset
+dados<-dados%>%select(sort(names(.)))
+names(dados)<-c("Instância","mês","quant","soma","Unidade")
+dados<-dados%>%select(mês,quant,soma,Instância)
+dados$mês<-as.numeric(dados$mês)
+dados$quant<-as.numeric(dados$quant)
+dados$quant[is.na(dados$quant)]=0
+dados$soma<-as.numeric(dados$soma)
+dados$soma[is.na(dados$soma)]=0
+dados<-as.data.frame(dados)
+
+#####meses=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
+
+meses<-1:12
+combin<-data.frame(mês=meses,quant=0,soma=0)
+
+#trt primeira instância
+inst1_tmdp<-dados%>%filter(Instância=="Primeira")%>%group_by(mês)%>%summarise(quant=sum(quant), soma=sum(soma))
+
+pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(inst1_tmdp$mês))))
+inst1_tmdp<-rbind(inst1_tmdp,combin[pos,])%>%arrange(mês)
+
+inst1_tmdp$Instância<-"Primeira"
+
+
+
+#Grau de cumprimento acumulado
+inst1_tmdp<-inst1_tmdp%>%mutate(GC_acumulado=cumsum(soma)/cumsum(quant))
+
+#Grau de cumprimento mensal
+inst1_tmdp<-inst1_tmdp%>%mutate(GC_mensal=soma/quant)
+
+#Grau de cumprimento atual
+inst1_tmdp<-inst1_tmdp%>%mutate(GC_atual=last(GC_acumulado))
+
+
+##trt segunda instância
+inst2_tmdp<-dados%>%filter(Instância=="Segunda")%>%group_by(mês)%>%summarise(quant=sum(quant), soma=sum(soma))
+
+pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(inst2_tmdp$mês))))
+inst2_tmdp<-rbind(inst2_tmdp,combin[pos,])%>%arrange(mês)
+
+inst2_tmdp$Instância<-"Segunda"
+
+
+#Grau de cumprimento acumulado
+inst2_tmdp<-inst2_tmdp%>%mutate(GC_acumulado=(cumsum(soma)/cumsum(quant)))
+
+#Grau de cumprimento mensal
+inst2_tmdp<-inst2_tmdp%>%mutate(GC_mensal=(soma/quant))
+
+#Grau de cumprimento atual
+inst2_tmdp<-inst2_tmdp%>%mutate(GC_atual=last(GC_acumulado))
+
+
+#trt total
+trt_total<-dados%>%group_by(mês)%>%summarise(quant=sum(quant), soma=sum(soma))
+
+pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(trt_total$mês))))
+trt_total<-rbind(trt_total,combin[pos,])%>%arrange(mês)
+
+trt_total$Instância<-"TRT total"
+
+#Grau de cumprimento acumulado
+trt_total<-trt_total%>%mutate(GC_acumulado=cumsum(soma)/cumsum(quant))
+
+#Grau de cumprimento mensal
+trt_total<-trt_total%>%mutate(GC_mensal=soma/quant)
+
+#Grau de cumprimento atual
+trt_total<-trt_total%>%mutate(GC_atual=last(GC_acumulado))
+
+
+
+final<-rbind(inst1_tmdp,inst2_tmdp,trt_total)
+
+
+meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+
+
+aux=1:dim(final)[1]
+
+
+for(i in 1:max(final$mês)){
+  a=which(final$mês==i)
+  aux[a]=meses[i]
+}
+
+
+final$mes_nomes=aux
+
+
+final<-final%>%mutate(meta=case_when(Instância=="Primeira"~148, Instância=="Segunda"~137, Instância=="TRT total"~140))
+
+
+
+final$GC_tmdp2<-final$meta/final$GC_acumulado
+
+final<-as.data.frame(final)
---
+++ TMDP/tmdp1 script.R
@@ -0,0 +1,84 @@
+# 'dataset' tem os dados de entrada para este script
+
+#TMDP 1
+
+require(dplyr)
+
+
+dados<-dataset
+
+dados<-dados%>%select(sort(names(.)))
+names(dados)<-c("Instância","mês","quant","soma","unidade")
+dados$mês<-as.numeric(dados$mês)
+dados$quant<-as.numeric(dados$quant)
+dados$quant[is.na(dados$quant)]=0
+dados$soma<-as.numeric(dados$soma)
+dados<-dados%>%select(unidade,mês,quant,soma)
+dados<-dados[-c(1:40),]
+dados<-as.data.frame(dados)
+Unidades<-as.data.frame(dados%>%select(unidade)); Unidades<-Unidades[c(1:37,39),]
+
+
+#trt primeira instância por mês
+
+TRT_1<-cbind(unidade=rep(".TRT 7 1ª INSTÂNCIA",length(unique(dados$mês))),
+             dados%>%group_by(mês)%>%summarise(quant=sum(quant),soma=sum(soma)))
+dados2<-rbind(dados,TRT_1)
+dados2<-arrange(dados2,unidade,mês)
+
+
+
+require(lubridate)
+require(data.table)
+
+
+#mes=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
+mês=1:12 
+combin=CJ(unidade=Unidades,mês) #combinação das unidades com cada mês para a comparação
+combin$quant<-0
+combin$soma<-0
+combin<-as.data.frame(combin)
+
+
+require(prodlim)
+
+
+#ajeitar o combin[1:2] para combin[,1:2]
+pos=which(is.na(row.match(combin[,1:2],dados2[,1:2]))) #linhas para adicionar
+ee=rbind(dados2,combin[pos,]) #juntando o data frame com as linhas faltantes
+
+#reorganizar as linhas por unidade e mês
+ee=ee%>%arrange(unidade,mês)
+
+dados2=as.data.frame(ee)
+
+
+#divididos por 100 para ficar em porcentagem corretamente no power bi
+#Grau de cumprimento acumulado
+dados2<-dados2%>%group_by(unidade)%>%mutate(GC_acumulado=cumsum(soma)/cumsum(quant))
+
+#Grau de cumprimento mensal
+dados2<-dados2%>%mutate(GC_mensal=soma/quant)
+
+#Grau de cumprimento atual
+dados2<-dados2%>%group_by(unidade)%>%mutate(GC_atual=last(GC_acumulado))
+
+dados2[is.na(dados2)]=0
+
+
+meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+
+aux=1:dim(dados2)[1]
+
+
+for(i in 1:max(dados2$mês)){
+  a=which(dados2$mês==i)
+  aux[a]=meses[i]
+}
+
+
+dados2$mes_nomes=aux
+
+
+dados2$meta<-148
+dados2$gc_tmdp1<-148/dados2$GC_acumulado

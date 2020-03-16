a33244c61fccb2f2bb4f33ac79d26c2fc6aab86c
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-03-16 15:43:31 -0300

    Update Meta resumo 1.R

--- Meta 1/Meta resumo 1.R
+++ Meta 1/Meta resumo 1.R
@@ -7,21 +7,21 @@ require(dplyr)
 dados<-dataset
 
 dados<-dados%>%select(sort(names(.)))
-names(dados)<-c("Instância","mês","Pergunta","quant","Unidade")
-dados<-dados%>%select(mês,Pergunta,Instância,quant)
+names(dados)<-c("InstÃ¢ncia","mÃªs","Pergunta","quant","Unidade")
+dados<-dados%>%select(mÃªs,Pergunta,InstÃ¢ncia,quant)
 
 
-#Primeira Instância
-inst1_p11<-dados%>%filter(Pergunta=="P11" & Instância=="Primeira")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst1_p11)<-c("mês","P11")
-inst1_p13<-dados%>%filter(Pergunta=="P13" & Instância=="Primeira")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst1_p13)<-c("mês","P13")
-inst1_p17<-dados%>%filter(Pergunta=="P17" & Instância=="Primeira")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst1_p17)<-c("mês","P17")
-inst1_p19<-dados%>%filter(Pergunta=="P19" & Instância=="Primeira")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst1_p19)<-c("mês","P19")
+#Primeira InstÃ¢ncia
+inst1_p11<-dados%>%filter(Pergunta=="P11" & InstÃ¢ncia=="Primeira")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst1_p11)<-c("mÃªs","P11")
+inst1_p13<-dados%>%filter(Pergunta=="P13" & InstÃ¢ncia=="Primeira")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst1_p13)<-c("mÃªs","P13")
+inst1_p17<-dados%>%filter(Pergunta=="P17" & InstÃ¢ncia=="Primeira")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst1_p17)<-c("mÃªs","P17")
+inst1_p19<-dados%>%filter(Pergunta=="P19" & InstÃ¢ncia=="Primeira")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst1_p19)<-c("mÃªs","P19")
 
-primeira_inst<-full_join(inst1_p11, inst1_p13, by="mês")%>%full_join(.,inst1_p17, by="mês")%>%full_join(.,inst1_p19, by="mês")
+primeira_inst<-full_join(inst1_p11, inst1_p13, by="mÃªs")%>%full_join(.,inst1_p17, by="mÃªs")%>%full_join(.,inst1_p19, by="mÃªs")
 primeira_inst[is.na(primeira_inst)]=0
 
 
@@ -29,16 +29,16 @@ require(lubridate)
 require(data.table)
 ##############################
 ############################
-#####meses=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
-meses<-1:12
-combin<-data.frame(mês=meses,P11=rep(0,length(meses)),P13=rep(0,length(meses)),P17=rep(0,length(meses)),P19=rep(0,length(meses)))
+
+meses=1:month(floor_date(Sys.Date() - months(1), "month")) # atÃ© o mÃªs anterior ao atual
+combin<-data.frame(mÃªs=meses,P11=rep(0,length(meses)),P13=rep(0,length(meses)),P17=rep(0,length(meses)),P19=rep(0,length(meses)))
 
 
 require(prodlim)
-pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(primeira_inst$mês))))
-primeira_inst<-rbind(primeira_inst,combin[pos,])%>%arrange(mês)
+pos=which(is.na(row.match(as.data.frame(combin$mÃªs),as.data.frame(primeira_inst$mÃªs))))
+primeira_inst<-rbind(primeira_inst,combin[pos,])%>%arrange(mÃªs)
 
-primeira_inst$Instância<-rep("Primeira",dim(primeira_inst)[1])
+primeira_inst$InstÃ¢ncia<-rep("Primeira",dim(primeira_inst)[1])
 
 #multiplicado por 100 para colocar em porcentagem no power bi
 #Grau de cumprimento acumulado
@@ -55,27 +55,27 @@ primeira_inst[is.na(primeira_inst)]=0
 
 
 
-#Segunda instância
-inst2_p11<-dados%>%filter(Pergunta=="P11" & Instância=="Segunda")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst2_p11)<-c("mês","P11")
-inst2_p13<-dados%>%filter(Pergunta=="P13" & Instância=="Segunda")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst2_p13)<-c("mês","P13")
-inst2_p15<-dados%>%filter(Pergunta=="P15" & Instância=="Segunda")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst2_p15)<-c("mês","P15")
-inst2_p17<-dados%>%filter(Pergunta=="P17" & Instância=="Segunda")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst2_p17)<-c("mês","P17")
-inst2_p19<-dados%>%filter(Pergunta=="P19" & Instância=="Segunda")%>%group_by(mês)%>%summarise(quant=sum(quant))
-names(inst2_p19)<-c("mês","P19")
+#Segunda instÃ¢ncia
+inst2_p11<-dados%>%filter(Pergunta=="P11" & InstÃ¢ncia=="Segunda")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst2_p11)<-c("mÃªs","P11")
+inst2_p13<-dados%>%filter(Pergunta=="P13" & InstÃ¢ncia=="Segunda")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst2_p13)<-c("mÃªs","P13")
+inst2_p15<-dados%>%filter(Pergunta=="P15" & InstÃ¢ncia=="Segunda")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst2_p15)<-c("mÃªs","P15")
+inst2_p17<-dados%>%filter(Pergunta=="P17" & InstÃ¢ncia=="Segunda")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst2_p17)<-c("mÃªs","P17")
+inst2_p19<-dados%>%filter(Pergunta=="P19" & InstÃ¢ncia=="Segunda")%>%group_by(mÃªs)%>%summarise(quant=sum(quant))
+names(inst2_p19)<-c("mÃªs","P19")
 
-segunda_inst<-full_join(inst2_p11, inst2_p13, by="mês")%>%full_join(.,inst2_p15, by="mês")%>%full_join(.,inst2_p17, by="mês")%>%full_join(.,inst2_p19, by="mês")
+segunda_inst<-full_join(inst2_p11, inst2_p13, by="mÃªs")%>%full_join(.,inst2_p15, by="mÃªs")%>%full_join(.,inst2_p17, by="mÃªs")%>%full_join(.,inst2_p19, by="mÃªs")
 segunda_inst[is.na(segunda_inst)]=0
 
 
 
-combin2<-data.frame(mês=meses,P11=rep(0,length(meses)),P13=rep(0,length(meses)),P15=rep(0,length(meses)),P17=rep(0,length(meses)),P19=rep(0,length(meses)))
-pos2=which(is.na(row.match(as.data.frame(combin2$mês),as.data.frame(segunda_inst$mês))))
-segunda_inst<-rbind(segunda_inst,combin2[pos2,])%>%arrange(mês)
-segunda_inst$Instância<-rep("Segunda",dim(segunda_inst)[1])
+combin2<-data.frame(mÃªs=meses,P11=rep(0,length(meses)),P13=rep(0,length(meses)),P15=rep(0,length(meses)),P17=rep(0,length(meses)),P19=rep(0,length(meses)))
+pos2=which(is.na(row.match(as.data.frame(combin2$mÃªs),as.data.frame(segunda_inst$mÃªs))))
+segunda_inst<-rbind(segunda_inst,combin2[pos2,])%>%arrange(mÃªs)
+segunda_inst$InstÃ¢ncia<-rep("Segunda",dim(segunda_inst)[1])
 
 
 #Grau de cumprimento acumulado
@@ -91,7 +91,7 @@ segunda_inst[is.na(segunda_inst)]=0
 
 #TRT total
 primeira_inst2<-primeira_inst%>%mutate(P15=rep(0,dim(primeira_inst)[1]))
-TRT_total<-rbind(primeira_inst2, segunda_inst)%>%group_by(mês)%>%summarise(P11=sum(P11),P13=sum(P13),P15=sum(P15),P17=sum(P17),P19=sum(P19))
+TRT_total<-rbind(primeira_inst2, segunda_inst)%>%group_by(mÃªs)%>%summarise(P11=sum(P11),P13=sum(P13),P15=sum(P15),P17=sum(P17),P19=sum(P19))
 
 #Grau de cumprimento acumulado
 TRT_total<-TRT_total%>%mutate(GCacumulado=(cumsum(P13)/(cumsum(P11)+1-cumsum(P15)+cumsum(P17)-cumsum(P19))))
@@ -102,7 +102,7 @@ TRT_total<-TRT_total%>%mutate(GCmensal=(P13/(P11+1-P15+P17-P19)))
 #Grau de cumprimento atual
 TRT_total$GCatual<-rep(last(TRT_total$GCacumulado),dim(TRT_total)[1])
 
-TRT_total$Instância<-"TRT total"
+TRT_total$InstÃ¢ncia<-"TRT total"
 
 TRT_total[is.na(TRT_total)]=0
 
@@ -110,14 +110,14 @@ TRT_total[is.na(TRT_total)]=0
 Final<-rbind(primeira_inst2,segunda_inst,TRT_total)
 
 
-meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+meses=c("janeiro","fevereiro","marÃ§o","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
 
 
 aux=1:dim(Final)[1]
 
 
-for(i in 1:max(Final$mês)){
-  a=which(Final$mês==i)
+for(i in 1:max(Final$mÃªs)){
+  a=which(Final$mÃªs==i)
   aux[a]=meses[i]
 }
 

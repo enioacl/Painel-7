cef47adc57055dd5c2afd68f39dc8ec3b57fed9b
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-04-16 13:45:39 -0300

    Update tmdp resumo.R

--- Meta 7/tmdp resumo.R
+++ Meta 7/tmdp resumo.R
@@ -7,27 +7,27 @@ require(lubridate)
 
 dados<-dataset
 dados<-dados%>%select(sort(names(.)))
-names(dados)<-c("Instância","mês","quant","soma","Unidade")
-dados<-dados%>%select(mês,quant,soma,Instância)
-dados$mês<-as.numeric(dados$mês)
+names(dados)<-c("InstÃ¢ncia","mÃªs","quant","soma","Unidade")
+dados<-dados%>%select(mÃªs,quant,soma,InstÃ¢ncia)
+dados$mÃªs<-as.numeric(dados$mÃªs)
 dados$quant<-as.numeric(dados$quant)
 dados$quant[is.na(dados$quant)]=0
 dados$soma<-as.numeric(dados$soma)
 dados$soma[is.na(dados$soma)]=0
 dados<-as.data.frame(dados)
 
-#####meses=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
 
-meses<-1:12
-combin<-data.frame(mês=meses,quant=0,soma=0)
+meses=1:month(floor_date(Sys.Date() - months(1), "month")) # atÃ© o mÃªs anterior ao atual
+#meses<-1:12
+combin<-data.frame(mÃªs=meses,quant=0,soma=0)
 
-#trt primeira instância
-inst1_tmdp<-dados%>%filter(Instância=="Primeira")%>%group_by(mês)%>%summarise(quant=sum(quant), soma=sum(soma))
+#trt primeira instÃ¢ncia
+inst1_tmdp<-dados%>%filter(InstÃ¢ncia=="Primeira")%>%group_by(mÃªs)%>%summarise(quant=sum(quant), soma=sum(soma))
 
-pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(inst1_tmdp$mês))))
-inst1_tmdp<-rbind(inst1_tmdp,combin[pos,])%>%arrange(mês)
+pos=which(is.na(row.match(as.data.frame(combin$mÃªs),as.data.frame(inst1_tmdp$mÃªs))))
+inst1_tmdp<-rbind(inst1_tmdp,combin[pos,])%>%arrange(mÃªs)
 
-inst1_tmdp$Instância<-"Primeira"
+inst1_tmdp$InstÃ¢ncia<-"Primeira"
 
 
 
@@ -41,13 +41,13 @@ inst1_tmdp<-inst1_tmdp%>%mutate(GC_mensal=soma/quant)
 inst1_tmdp<-inst1_tmdp%>%mutate(GC_atual=last(GC_acumulado))
 
 
-##trt segunda instância
-inst2_tmdp<-dados%>%filter(Instância=="Segunda")%>%group_by(mês)%>%summarise(quant=sum(quant), soma=sum(soma))
+##trt segunda instÃ¢ncia
+inst2_tmdp<-dados%>%filter(InstÃ¢ncia=="Segunda")%>%group_by(mÃªs)%>%summarise(quant=sum(quant), soma=sum(soma))
 
-pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(inst2_tmdp$mês))))
-inst2_tmdp<-rbind(inst2_tmdp,combin[pos,])%>%arrange(mês)
+pos=which(is.na(row.match(as.data.frame(combin$mÃªs),as.data.frame(inst2_tmdp$mÃªs))))
+inst2_tmdp<-rbind(inst2_tmdp,combin[pos,])%>%arrange(mÃªs)
 
-inst2_tmdp$Instância<-"Segunda"
+inst2_tmdp$InstÃ¢ncia<-"Segunda"
 
 
 #Grau de cumprimento acumulado
@@ -61,12 +61,12 @@ inst2_tmdp<-inst2_tmdp%>%mutate(GC_atual=last(GC_acumulado))
 
 
 #trt total
-trt_total<-dados%>%group_by(mês)%>%summarise(quant=sum(quant), soma=sum(soma))
+trt_total<-dados%>%group_by(mÃªs)%>%summarise(quant=sum(quant), soma=sum(soma))
 
-pos=which(is.na(row.match(as.data.frame(combin$mês),as.data.frame(trt_total$mês))))
-trt_total<-rbind(trt_total,combin[pos,])%>%arrange(mês)
+pos=which(is.na(row.match(as.data.frame(combin$mÃªs),as.data.frame(trt_total$mÃªs))))
+trt_total<-rbind(trt_total,combin[pos,])%>%arrange(mÃªs)
 
-trt_total$Instância<-"TRT total"
+trt_total$InstÃ¢ncia<-"TRT total"
 
 #Grau de cumprimento acumulado
 trt_total<-trt_total%>%mutate(GC_acumulado=cumsum(soma)/cumsum(quant))
@@ -82,14 +82,14 @@ trt_total<-trt_total%>%mutate(GC_atual=last(GC_acumulado))
 final<-rbind(inst1_tmdp,inst2_tmdp,trt_total)
 
 
-meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+meses=c("janeiro","fevereiro","marÃ§o","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
 
 
 aux=1:dim(final)[1]
 
 
-for(i in 1:max(final$mês)){
-  a=which(final$mês==i)
+for(i in 1:max(final$mÃªs)){
+  a=which(final$mÃªs==i)
   aux[a]=meses[i]
 }
 
@@ -97,7 +97,7 @@ for(i in 1:max(final$mês)){
 final$mes_nomes=aux
 
 
-final<-final%>%mutate(meta=case_when(Instância=="Primeira"~148, Instância=="Segunda"~137, Instância=="TRT total"~140))
+final<-final%>%mutate(meta=case_when(InstÃ¢ncia=="Primeira"~148, InstÃ¢ncia=="Segunda"~137, InstÃ¢ncia=="TRT total"~140))
 
 
 

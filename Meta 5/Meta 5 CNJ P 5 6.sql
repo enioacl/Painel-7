ab07dea80fab08b2d94fd9b8c8cce8f7744942a0
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-04-16 16:49:40 -0300

    Update Meta 5 CNJ P 5 6.sql

--- Meta 5/Meta 5 CNJ P 5 6.sql
+++ Meta 5/Meta 5 CNJ P 5 6.sql
@@ -40,8 +40,10 @@ ORDER BY NUM_ORGAO_ESTATISTICA) vt ON vt.NUM_ORGAO_ESTATISTICA = processo.num_or
 INNER JOIN (SELECT rem.NUM_REMESSA, max(rem.NUM_LOTE) NUM_LOTE
               FROM eg.EGT_REMESSA_LOTE rem
         WHERE rem.DTA_INICIO_PERIODO_REFERENCIA BETWEEN 
-            TO_DATE('01/01/2019','dd/mm/yy') AND 
-            TO_DATE('31/12/2019','dd/mm/yy')   
+            TO_DATE((select ADD_MONTHS(trunc(sysdate,'mm'),-extract(MONTH from sysdate)+1) from dual),'dd/mm/yy') AND 
+            TO_DATE((select ADD_MONTHS(last_day(sysdate), -1) from dual ),'dd/mm/yy')
+	    --TO_DATE('01/01/2019','dd/mm/yy') AND 
+            --TO_DATE('31/12/2019','dd/mm/yy')   
             AND rem.COD_SITUACAO_REMESSA = 'G'             
 GROUP BY rem.NUM_REMESSA) x ON processo.NUM_REMESSA=x.NUM_REMESSA AND processo.NUM_LOTE=x.NUM_LOTE
 LEFT JOIN eg.EGT_REMESSA_LOTE remessa ON (remessa.NUM_TRIBUNAL = PROCESSO.NUM_TRIBUNAL AND 
@@ -50,17 +52,9 @@ LEFT JOIN eg.EGT_REMESSA_LOTE remessa ON (remessa.NUM_TRIBUNAL = PROCESSO.NUM_TR
 												remessa.NUM_LOTE = PROCESSO.NUM_LOTE)
 WHERE
 	processo.NUM_TRIBUNAL = 7
-        -- G - Gerada
-	--AND REMESSA.COD_SITUACAO_REMESSA = 'G'
-	-- M - Mensal
 	AND REMESSA.COD_PERIODICIDADE = 'M'
-	-- Se desejar, faça filtro por orgaos estatistica aqui
 	AND processo.NUM_ORGAO_ESTATISTICA IN (SELECT NUM_ORGAO_ESTATISTICA FROM eg.EGT_ORGAO_ESTATISTICA)
-	-- Filtro por remessa e lote (se desejar, filtre por mes da remessa de maneira similar ao SQL que recupera remessa e lote a partir do mes)
-	-- Apenas itens com detalhes de processos 
-    --AND proc.ANO_PROC <=2017
 	AND estrutura_item.num_tipo_complemento = 1
-	-- Filtro por item - Exemplo com indicadores 90074 e 92198
 	AND classe.NUM_CLASSE_CNJ IN (1116)
     AND processo.NUM_ITEM IN (391, 90391, 91, 90091)
     

0a18b8caf9c94b192bc393ca0ec12949e7a2ce74
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-09-16 18:00:59 -0300

    Update Residuo_P27.sql

--- Meta 2/Residuo_P27.sql
+++ Meta 2/Residuo_P27.sql
@@ -1,7 +1,7 @@
 --P2.7 1° grau
 SELECT  
 saida.txt_unidade,
-saida.mes,
+saida.DATA,
 saida.PROCESSO_NUMERO_UNICO
 --saida.NOM_CLASSE
 FROM (SELECT  
@@ -21,7 +21,8 @@ FROM (SELECT
 		processo.DTA_OCORRENCIA,    
         proc.ANO_PROC,
         2018-proc.ANO_PROC AS Idade_do_processo_em_anos,
-        extract(MONTH from remessa.DTA_INICIO_PERIODO_REFERENCIA) AS mes
+        --extract(MONTH from remessa.DTA_INICIO_PERIODO_REFERENCIA) AS mes
+      remessa.DTA_INICIO_PERIODO_REFERENCIA as DATA
 FROM eg.egt_info_processo processo
 LEFT JOIN eg.EGT_PROCESSO proc ON (	proc.NUM_TRIBUNAL = processo.NUM_TRIBUNAL 
 							AND proc.NUM_ORGAO_ESTATISTICA = processo.NUM_ORGAO_ESTATISTICA 

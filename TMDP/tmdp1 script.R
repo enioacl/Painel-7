2efc00d34bbfce0ac3f826224632416939e67c53
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-10-01 21:05:44 -0300

    Update tmdp1 script.R

--- TMDP/tmdp1 script.R
+++ TMDP/tmdp1 script.R
@@ -2,21 +2,54 @@
 
 #TMDP 1
 
-require(dplyr)
+library(dplyr)
+library(lubridate)
 
 
 dados<-dataset
 
 dados<-dados%>%select(sort(names(.)))
-names(dados)<-c("Instância","mês","quant","soma","unidade")
+names(dados)<-c("Instância","mês","Pergunta","quant","soma","unidade")
+dados<-as.data.frame(dados)
+Unidades<-as.data.frame(dados%>%select(unidade)); Unidades<-Unidades[c(1:37,39),]
+
+
+
+
+#SUBSTITUI AS VT's DOS PROCESSOS QUE FORAM REDISTRIBUÍDOS
+redis<-filter(dados,(Pergunta=="REDISTRIBUIDO"))%>%select(unidade,mês,quant)
+redis$mês<-dmy_hms(redis$mês)
+dados<-as.data.frame(dados[-c(1:40),])
+dados$Pergunta[is.na(dados$Pergunta)]="deixar"
+dados<-filter(dados,!(Pergunta=="REDISTRIBUIDO"))
+
+
+#DEIXAR APENAS A ÚLTIMA VT PARA A QUAL O PROCESSO FOI DISTRIBUÍDO
+
+redis<-redis%>%group_by(quant)%>%mutate(mês=if_else(mês!=max(mês),as.Date(NA),mês))
+
+redis<-na.omit(redis)
+redis<-select(redis,unidade,quant)
+
+a<-left_join(dados,redis,by="quant")
+a$unidade.x<-ifelse(is.na(a$unidade.y),a$unidade.x,a$unidade.y)
+a<-select(a,-unidade.y)
+a<-a%>%select(unidade.x,mês,quant,soma,Instância)
+
+names(a)[1]="unidade"
+
+dados<-a
+
+dados<-dados%>%group_by(unidade,mês)%>%summarise(quant=n(),soma=sum(soma))%>%data.frame()%>%
+  select(unidade,mês,quant,soma)
+
+
+
 dados$mês<-as.numeric(dados$mês)
 dados$quant<-as.numeric(dados$quant)
 dados$quant[is.na(dados$quant)]=0
 dados$soma<-as.numeric(dados$soma)
-dados<-dados%>%select(unidade,mês,quant,soma)
-dados<-dados[-c(1:40),]
-dados<-as.data.frame(dados)
-Unidades<-as.data.frame(dados%>%select(unidade)); Unidades<-Unidades[c(1:37,39),]
+
 
 
 #trt primeira instância por mês
@@ -28,19 +61,18 @@ dados2<-arrange(dados2,unidade,mês)
 
 
 
-require(lubridate)
-require(data.table)
+library(lubridate)
+library(data.table)
 
 
 mês=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
-#mês=1:12 
 combin=CJ(unidade=Unidades,mês) #combinação das unidades com cada mês para a comparação
 combin$quant<-0
 combin$soma<-0
 combin<-as.data.frame(combin)
 
 
-require(prodlim)
+library(prodlim)
 
 
 #ajeitar o combin[1:2] para combin[,1:2]
@@ -63,7 +95,7 @@ dados2<-dados2%>%mutate(GC_mensal=soma/quant)
 #Grau de cumprimento atual
 dados2<-dados2%>%group_by(unidade)%>%mutate(GC_atual=last(GC_acumulado))
 
-dados2[is.na(dados2)]=0
+dados2[is.na(dados2)]=1
 
 
 meses=c("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")

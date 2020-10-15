b00f70fa093eca6d1b50b954d8f816c40c79e973
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-10-15 15:02:34 -0300

    Update Meta 2 P 2 4 1º grau.sql

--- Meta 2/Meta 2 P 2 4 1º grau.sql
+++ Meta 2/Meta 2 P 2 4 1º grau.sql
@@ -2,7 +2,7 @@
 SELECT  
 saida.txt_unidade,
 saida.mes,                                    
-count(saida.NUM_INTERNO_PROCESSO) AS quantidade
+saida.PROCESSO_NUMERO_UNICO AS quantidade
 FROM (SELECT  
 		processo.NUM_REMESSA,
 		processo.NUM_LOTE,
@@ -100,7 +100,7 @@ WHERE
   
 
 ORDER BY processo.NUM_REMESSA, processo.NUM_LOTE, processo.num_item, processo.NUM_ORGAO_ESTATISTICA) saida
-group by saida.txt_unidade, saida.mes
+--group by saida.txt_unidade, saida.mes
 order by saida.txt_unidade, saida.mes
 
 

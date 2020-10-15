7cf4e4db84bba899663edbd3ee5f2e7549e95fc1
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-10-15 15:55:37 -0300

    Update Residuo_P24.sql

--- Meta 2/Residuo_P24.sql
+++ Meta 2/Residuo_P24.sql
@@ -1,7 +1,7 @@
 --2.4 1° grau
 SELECT  
 saida.txt_unidade,
-saida.DATA,                                    
+saida.mes,                                    
 saida.PROCESSO_NUMERO_UNICO
 FROM (SELECT  
 		processo.NUM_REMESSA,
@@ -17,10 +17,10 @@ FROM (SELECT
 		classe.TXT_SIGLA_CLASSE,
 		classe.NOM_CLASSE,
         vt.TXT_UNIDADE,
-		processo.DTA_OCORRENCIA as DATA,    
+		processo.DTA_OCORRENCIA as mes,    
         proc.ANO_PROC,
         2018-proc.ANO_PROC AS Idade_do_processo_em_anos,
-        extract(MONTH from remessa.DTA_INICIO_PERIODO_REFERENCIA) AS mes
+        extract(MONTH from remessa.DTA_INICIO_PERIODO_REFERENCIA) 
       
 FROM eg.egt_info_processo processo
 LEFT JOIN eg.EGT_PROCESSO proc ON (	proc.NUM_TRIBUNAL = processo.NUM_TRIBUNAL 

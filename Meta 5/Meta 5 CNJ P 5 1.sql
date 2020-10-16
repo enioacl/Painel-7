4aeb1d6ef45e7f5eed701c1f8736733433ffd998
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-10-16 11:44:56 -0300

    Update Meta 5 CNJ P 5 1.sql

--- Meta 5/Meta 5 CNJ P 5 1.sql
+++ Meta 5/Meta 5 CNJ P 5 1.sql
@@ -4,7 +4,7 @@
 SELECT  
 saida.TXT_unidade,
 saida.mes,
-count(DISTINCT(saida.PROCESSO_NUMERO_UNICO)) AS quantidade
+DISTINCT(saida.PROCESSO_NUMERO_UNICO) AS quantidade
 --saida.NOM_CLASSE
 FROM (SELECT  
 		processo.NUM_REMESSA,
@@ -102,5 +102,5 @@ WHERE
     )
 
 ORDER BY processo.NUM_REMESSA, processo.NUM_LOTE, processo.num_item, processo.NUM_ORGAO_ESTATISTICA) saida
-group by saida.txt_unidade, saida.mes
+--group by saida.txt_unidade, saida.mes
 order by saida.txt_unidade, saida.mes

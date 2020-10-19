9156368530b5fb4327620fe5ce131147fd3c6cd6
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-10-19 15:04:10 -0300

    Update Meta 7 perguntas final.R

--- Meta 7/Meta 7 perguntas final.R
+++ Meta 7/Meta 7 perguntas final.R
@@ -1,3 +1,4 @@
+
 # 'dataset' tem os dados de entrada para este script
 dados<-dataset
 library(dplyr)
@@ -5,17 +6,59 @@ library(prodlim)
 library(data.table)
 library(lubridate)
 
-#dados<-read.table(file.choose(),  sep ="\t", header = TRUE)
 
 dados<-dados%>%select(sort(names(.)))
-names(dados)[c(1,2,5,6)]<-c("Instância","mes","pergunta","unidade")
-dados<-dados%>%select(unidade,mes,Instância,pergunta)
+names(dados)[c(1,2,3,4,6)]<-c("Instância","mes","pergunta","quant","unidade")
+dados<-dados%>%select(unidade,mes,Instância,pergunta,quant)
 dados$unidade<-as.character(dados$unidade)
-dados$mes<-as.numeric(dados$mes)
+
+dados$unidade[dados$unidade=="VT DE ARACATI"]="01ª VT DE ARACATI"
+dados$unidade[dados$unidade=="VT DE EUSÉBIO"]="01ª VT DE EUSEBIO"
+dados$unidade[dados$unidade=="VT DE PACAJUS"]="01ª VT DE PACAJUS"
+dados$unidade[dados$unidade=="VT DE TIANGUÁ"]="01ª VT DE TIANGUÁ"
+dados$unidade[dados$unidade=="VT DE BATURITE"]="01ª VT DE BATURITE"
+dados$unidade[dados$unidade=="VT DE IGUATU"]="01ª VT DE IGUATU"
+dados$unidade[dados$unidade=="VT DE QUIXADA"]="01ª VT DE QUIXADA"
+dados$unidade[dados$unidade=="VT DE CRATEUS"]="01ª VT DE CRATEUS"
+dados$unidade[dados$unidade=="VT DE LIMOEIRO DO NORTE"]="01ª VT DE LIMOEIRO DO NORTE"
+dados$unidade[dados$unidade=="01ª VT DA REGIÃO DO CARIRI"]="01ª VT DE JUAZEIRO DO NORTE"
+dados$unidade[dados$unidade=="02ª VT DA REGIÃO DO CARIRI"]="02ª VT DE JUAZEIRO DO NORTE"
+dados$unidade[dados$unidade=="03ª VT DA REGIÃO DO CARIRI"]="03ª VT DE JUAZEIRO DO NORTE"
+dados$unidade[dados$unidade=="VT DE SÃO GONÇALO DO AMARANTE"]="01ª VT DE SAO GONCALO DO AMARANTE"
+dados$unidade[dados$unidade=="01ª VT DE MARACANAÚ"]="01ª VT DE MARACANAU"
+dados$unidade[dados$unidade=="02ª VT DE MARACANAÚ"]="02ª VT DE MARACANAU"
+
 unidade<-as.character(dados[c(1:38),1])
+
+
+
+#SUBSTITUI AS VT's DOS PROCESSOS QUE FORAM REDISTRIBUÍDOS
+redis<-filter(dados,(pergunta=="REDISTRIBUIDO"))%>%select(unidade,mes,quant)
+redis$mes<-dmy(redis$mes) #aqui mudar para dmy_hms
+dados<-filter(dados,!(pergunta=="REDISTRIBUIDO"))
+dados$mes<-as.numeric(dados$mes)
+
+# #DEIXAR APENAS A ÚLTIMA VT PARA A QUAL O PROCESSO FOI DISTRIBUÍDO
+
+redis<-redis%>%group_by(quant)%>%mutate(mes=if_else(mes!=max(mes),as.Date(NA),mes))
+redis<-na.omit(redis)
+redis<-select(redis,unidade,quant)
+
+
+a<-left_join(dados,redis,by="quant")
+a$unidade.x<-ifelse(is.na(a$unidade.y),a$unidade.x,a$unidade.y)
+a<-select(a,-unidade.y)
+names(a)[1]="unidade"
+
+dados<-a
+
+
+dados$mes<-as.numeric(dados$mes)
+
 
 P73<-dados%>%group_by(unidade)%>%filter(pergunta=='P7.3')%>%summarise(quantidade=length(unidade))%>%data.frame()
 names(P73)[2]<-"P73"
+P73$P73<-as.numeric(P73$P73)
 P73[nrow(P73)+1,]=c(".TRT 7 1ª INSTÂNCIA",sum(P73$P73))
 P73$P73<-as.numeric(P73$P73)
 
@@ -51,7 +94,6 @@ dados2[is.na(dados2)]=0
 
 #linhas faltantes
 
-#meses=1:12
 meses=1:month(floor_date(Sys.Date() - months(1), "month")) # até o mês anterior ao atual
 combin=CJ(unidade=unidade,mes=meses) #combinação das unidades com cada mês para a comparação
 combin$P74=rep(0,dim(combin)[1])
@@ -63,7 +105,6 @@ combin=as.data.frame(combin)
 combin$mes<-as.numeric(combin$mes)
 combin$unidade<-as.character(combin$unidade)
 
-require(prodlim)
 
 pos=which(is.na(row.match(combin[,1:2],dados2[,1:2]))) #linhas para adicionar
 ee=rbind(dados2,combin[pos,]) #juntando o data frame com as linhas faltantes
@@ -114,23 +155,6 @@ dados2$mes_nomes=aux
 
 dados2$Instância<-"TRT total"
 
-
-
-dados2$unidade[dados2$unidade=="VT DE ARACATI"]="01ª VT DE ARACATI"
-dados2$unidade[dados2$unidade=="VT DE EUSÉBIO"]="01ª VT DE EUSEBIO"
-dados2$unidade[dados2$unidade=="VT DE PACAJUS"]="01ª VT DE PACAJUS"
-dados2$unidade[dados2$unidade=="VT DE TIANGUÁ"]="01ª VT DE TIANGUÁ"
-dados2$unidade[dados2$unidade=="VT DE BATURITE"]="01ª VT DE BATURITE"
-dados2$unidade[dados2$unidade=="VT DE IGUATU"]="01ª VT DE IGUATU"
-dados2$unidade[dados2$unidade=="VT DE QUIXADA"]="01ª VT DE QUIXADA"
-dados2$unidade[dados2$unidade=="VT DE CRATEUS"]="01ª VT DE CRATEUS"
-dados2$unidade[dados2$unidade=="VT DE LIMOEIRO DO NORTE"]="01ª VT DE LIMOEIRO DO NORTE"
-dados2$unidade[dados2$unidade=="01ª VT DA REGIÃO DO CARIRI"]="01ª VT DE JUAZEIRO DO NORTE"
-dados2$unidade[dados2$unidade=="02ª VT DA REGIÃO DO CARIRI"]="02ª VT DE JUAZEIRO DO NORTE"
-dados2$unidade[dados2$unidade=="03ª VT DA REGIÃO DO CARIRI"]="03ª VT DE JUAZEIRO DO NORTE"
-dados2$unidade[dados2$unidade=="VT DE SÃO GONÇALO DO AMARANTE"]="01ª VT DE SAO GONCALO DO AMARANTE"
-dados2$unidade[dados2$unidade=="01ª VT DE MARACANAÚ"]="01ª VT DE MARACANAU"
-dados2$unidade[dados2$unidade=="02ª VT DE MARACANAÚ"]="02ª VT DE MARACANAU"
 
 
 dados2<-arrange(dados2,unidade,mes)

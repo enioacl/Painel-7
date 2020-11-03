9c3222119d77ddc6ccdfeb0d9ac9f12819dc33c1
Lorena Cândido <53229543+lorenacandido@users.noreply.github.com>
2020-11-03 13:55:21 -0300

    Update meta 3.R

--- Meta 3/meta 3.R
+++ Meta 3/meta 3.R
@@ -16,6 +16,7 @@ unidade<-as.data.frame(dados[c(1:37,39),1])
 #SUBSTITUI AS VT's DOS PROCESSOS QUE FORAM REDISTRIBUÍDOS
 redis<-filter(dados,(Pergunta=="REDISTRIBUIDO"))%>%select(unidade,mês,quant)
 redis$Unidade[redis$Unidade=="null"]=NA
+redis$quant[redis$quant=="null"]=NA
 redis$mês[redis$mês=="null"]=NA
 redis<-na.omit(redis)
 redis$mês<-dmy_hms(redis$mês)
